e66bf62795bb901d5cdb93e8f7b2d26afcd69230
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2022-11-30 16:03:33 -0500

    small change

--- 424Project/424Project.cpp
+++ 424Project/424Project.cpp
@@ -1,8 +1,7 @@
 // 424Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // 
-#include"Entity.h"
-#include"Sally.h"
-#include"Terrain.h"
+
+#include"engine.h"
 #include <iostream>
 
 static const int frameSize = 100;
